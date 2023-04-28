b80e7c1e1bb91baa317d49824843c9d3afcc8b42
Pedro Henrique Amorim de Azevedo <pedrohamorim2011@hotmail.com>
2023-04-28 15:21:37 -0300

    Recepção do Mega funcionando

--- Arduino-Mega-Slave/Arduino-Mega-Slave.ino
+++ Arduino-Mega-Slave/Arduino-Mega-Slave.ino
@@ -3,19 +3,19 @@ bool stringComplete = false;  // flag para indicar se a string está completa
 
 void setup() {
   Serial.begin(9600);
-  inputString.reserve(200); // reserva 200 bytes para a string de comando
-
+  inputString.reserve(20); // reserva 20 bytes para a string de comando
+  pinMode(13, OUTPUT);
+  Serial.println("Pronto!");
   // os pinos digitais serão setados a medida que formos precisando deles no projeto
 }
 
 void loop() {
   commitCmdString();
-  
 }
 
 void commitCmdString() {
   if (stringComplete) {
-    Serial.println(inputString);
+    parseCmdString();
     // clear the string:
     inputString = "";
     stringComplete = false;
@@ -27,19 +27,38 @@ void parseCmdString() {
   // A:15 leitura analógica da porta 15
   // ON:2 ligar pino 2
   // OFF:2 desligar pino 3
-  
+  uint8_t cmdIndex = inputString.indexOf(":");
+  String cmd = inputString.substring(0, cmdIndex);
+  uint8_t pin = inputString.substring(cmdIndex+1, inputString.length()).toInt();
+
+  if(cmd.equals("A")) {
+    Serial.println("Recebemos um A");
+    // A0 = GPIO 54 e A15 = GPIO 69
+    if(pin >= 54 && pin <= 69) {
+      Serial.println(analogRead(pin));
+    }
+  } 
+  else if(cmd.equals("OFF")) {
+    Serial.println("Recebemos um OFF");
+    digitalWrite(pin, LOW);
+  } 
+  else if (cmd.equals("ON")) {
+    Serial.println("Recebemos um ON");
+    digitalWrite(pin, HIGH);
+  }
+
+  //Serial.println(cmd);
+  //Serial.println(pin);
 }
 
 void serialEvent() {
   while (Serial.available()) {
     char inChar = (char)Serial.read();
-    
     // se o caractere for uma quebra de linha, dizemos que a string está pronta  
     if (inChar == '\n') {
       stringComplete = true;
     } else {
       inputString += inChar;
     }
-    
   }
 }
